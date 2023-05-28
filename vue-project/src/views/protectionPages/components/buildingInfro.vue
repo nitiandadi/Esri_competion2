0c00ff1f78a036948cad4aea25e89a839d4e0df9
Emperor Nitian <2178508639@qq.com>
2023-05-28 11:58:01 +0800

    1

---
+++ vue-project/src/views/protectionPages/components/buildingInfro.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="Container" ref="envirmentRef">
+        <div class="Container-panel-head">当前人数：</div>
+        <div class="Container-panel-body">
+            <div>入口1:人数{{ num1 }}</div>
+            <video src=""></video>
+        </div>
+        <div class="Container-panel-body">
+            <div>入口1:人数{{ num2 }}</div>
+            <video src=""></video>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+let num1 = ref(0);
+let num2 = ref(0);
+// import { ref, onMounted } from "vue";
+// //@ts-ignore
+// import { useViewStore } from '@/store/mapviewstore';
+// import Map from '@arcgis/core/Map';
+// import SceneLayer from '@arcgis/core/layers/SceneLayer';
+// import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+// import { SimpleRenderer } from "@arcgis/core/renderers";
+// import PolygonSymbol3D from "@arcgis/core/symbols/PolygonSymbol3D.js";
+// import PopupTemplate from "@arcgis/core/PopupTemplate";
+// onMounted(() => {
+//     let store = useViewStore();
+//     store.CreateSceneView();
+//     let view = store.getView() as __esri.SceneView;
+//     //set basemap for sceneview
+//     view.map = new Map({
+//         basemap: "satellite",
+//         ground: "world-elevation"
+//     });
+//     let symbol = new PolygonSymbol3D({
+//         symbolLayers: [
+//             {
+//                 type: "water",
+//                 waveDirection: 260,
+//                 color: "#25427c",
+//                 waveStrength: "moderate",
+//                 waterbodySize: "medium"
+//             }
+//         ]
+//     })
+//     let waterRender = new SimpleRenderer({
+//         symbol: symbol
+//     })
+//     let buildingLayer = new SceneLayer({
+//         url: "https://tiles.arcgis.com/tiles/R5nxHh77a68zEsEp/arcgis/rest/services/DuJiangYan/SceneServer"
+//     })
+//     let waterLayer = new FeatureLayer({
+//         url: "https://services7.arcgis.com/R5nxHh77a68zEsEp/arcgis/rest/services/dujiangyang2/FeatureServer",
+//         renderer: waterRender,
+//         elevationInfo: {
+//             mode: "on-the-ground",
+//             offset: 100
+//         },
+//     })
+//     view.when(() => {
+//         view.map.add(buildingLayer);
+//         view.map.add(waterLayer);
+//         // buildingLayer.when(() => {
+//         //     view.goTo(buildingLayer.fullExtent);
+//         // })
+//         waterLayer.when(() => {
+//             view.goTo(waterLayer.fullExtent);
+//             console.log('waterlayer');
+//             //    view.map.visible=false;
+//             //    view.visible = true;
+//             view.environment!.lighting!.directShadowsEnabled = true;
+//         })
+//         view.ui.add('navigation-toggle', 'top-left');
+//     })
+//     view.on("click", function (event) {
+//         // the hitTest() checks to see if any graphics in the view
+//         // intersect the given screen x, y coordinates
+//         view.hitTest(event)
+//             .then(function (response) {
+//                 const result = response.results[0] as __esri.SceneViewGraphicHit;
+
+//             });
+
+
+//         // Set the PopupTemplate on the selected graphic
+
+//     });
+
+// })
+</script>
+<style lang="scss" scoped>
+@import '@/style/protection.scss';
+</style>
---
+++ vue-project/src/views/protectionPages/index.vue
@@ -0,0 +1,164 @@
+<template>
+    <div ref="screenRef" class="screen">
+        <transition name="right-slide-in">
+            <el-card class="box-card" :style="{ left: showCard ? '0.5%' : '-380px' }">
+                <template #header>
+                    <span>景点保护</span>
+                </template>
+                <el-row>
+                    <el-col>
+                        <el-text>查看景点建筑</el-text>
+                        <el-switch v-model="switchList[0].value" size="large" class="mt-2" style="margin-left: 24px"
+                            inline-prompt :active-icon="Check" :inactive-icon="Close" @change="checkSwitch(0)" />
+                    </el-col>
+                    <el-col>
+                        <el-text>管理游客信息</el-text>
+                        <el-switch v-model="switchList[1].value" size="large" class="mt-2" style="margin-left: 24px"
+                            inline-prompt :active-icon="Check" :inactive-icon="Close" @change="checkSwitch(1)" />
+                    </el-col>
+                    <el-col>
+                        <el-text>游客类型分类</el-text>
+                        <el-switch v-model="switchList[2].value" size="large" class="mt-2" style="margin-left: 24px"
+                            inline-prompt :active-icon="Check" :inactive-icon="Close" @change="checkSwitch(2)" />
+                    </el-col>
+                </el-row>
+            </el-card>
+        </transition>
+        <buildingInfro v-if="switchList[0].value" />
+    </div>
+</template>
+    
+<script setup lang="ts">
+import { ElText, ElSwitch, ElRow, ElCol, ElCard } from 'element-plus';
+import { ref, onMounted } from 'vue'
+import { Check, Close } from '@element-plus/icons-vue'
+import { useScreen } from '@/hooks/useScreen';
+import buildingInfro from "./components/buildingInfro.vue";
+
+const value1 = ref(false)
+const value2 = ref(false)
+const value3 = ref(false)
+const screenRef = ref<HTMLElement | null>(null);
+useScreen(screenRef);
+let switchList = [value1, value2, value3];
+function checkSwitch(index: number) {
+    for (let i = 0; i < switchList.length; i++) {
+        if (i !== index) {
+            switchList[i].value = false;
+        }
+    }
+}
+const showCard = ref(false)
+// 触发卡片进入动画
+const triggerCardAnimation = () => {
+    showCard.value = true;
+};
+onMounted(() => {
+    console.log(screenRef.value?.clientHeight);
+    setTimeout(() => {
+        triggerCardAnimation();
+    }, 100);
+});
+</script>
+    
+<style lang="scss" >
+.screen {
+    transform-origin: 0 0;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transition: 0.3s;
+    background: rgba(255, 0, 0, 0.001);
+    overflow: hidden;
+    z-index: 2;
+}
+
+.el {
+    &-card {
+        position: fixed;
+        /* 或者使用 absolute */
+        top: 10%;
+        width: 13%;
+        height: 25%;
+        left: -380px;
+        border-radius: 6px;
+        /* 添加边角弧度 */
+        background-image: linear-gradient(-90deg, #182940 0%, #115687 100%);
+        /* 添加渐变 */
+        pointer-events: auto;
+        border: 0px solid var(--el-card-border-color);
+
+        &__header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 20%;
+            color: white;
+            font-size: 1.8rem;
+            /* 改变标题字体大小 */
+            font-weight: bold;
+            /* 加粗字体 */
+            font-style: italic;
+            /* 斜体字 */
+        }
+
+        &__body {
+            height: 80%;
+        }
+    }
+
+    &-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    &-col {
+        height: 33.33%;
+    }
+
+    &-text {
+        font-size: 1.4rem;
+        /* 改变标题字体大小 */
+        font-weight: bold;
+        /* 加粗字体 */
+        color: #92c3e4;
+    }
+
+    &-switch {
+        vertical-align: baseline;
+    }
+
+}
+
+/* 过渡动画 */
+.right-slide-in-enter-active,
+.right-slide-in-leave-active {
+    transition: all 0.5s ease-in-out;
+}
+
+.right-slide-in-enter {
+    opacity: 0;
+    transform: translateX(100%);
+}
+
+.right-slide-in-leave-to {
+    opacity: 0;
+    transform: translateX(100%);
+}
+
+.nihao {
+    position: relative;
+    top: 10%;
+    right: 10%;
+    width: 200px;
+    height: 200px;
+    border-radius: 6px;
+    /* 添加边角弧度 */
+    background-image: linear-gradient(-90deg, #182940 0%, #115687 100%);
+    /* 添加渐变 */
+    pointer-events: auto;
+    border: 0px solid var(--el-card-border-color);
+    margin-inline: 7px;
+}
+</style>
